3a80c6510bf2c15fcf9b04b18a727449ee57cc11
Stephen Cook <31995104+sjc5897@users.noreply.github.com>
2022-01-21 19:40:32 -0500

    Finished 82. removeDupsFromSortedList

---
+++ src/removeDupsFromSortedList.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+public class removeDupsFromSortedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head){
+        ListNode sentinel =   new ListNode(0,head);
+        ListNode processNode = sentinel;
+
+        while(head != null){
+            if(head.next != null && head.val == head.next.val){
+                while(head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                processNode.next = head.next;
+            }else{
+                processNode = processNode.next;
+            }
+            head = head.next;
+        }
+        return sentinel.next;
+    }
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(4, new ListNode(5)))))));
+
+        System.out.println(deleteDuplicates(head));
+
+    }
+
+
+}
