c320d67aeda9e6a273d6c1b32e6968170876ff6d
Stephen Cook <stevejcook114@gmail.com>
2022-01-23 09:09:05 -0500

    Finished 3sum

--- src/two_pointers/threeSum.java
+++ src/two_pointers/threeSum.java
@@ -6,28 +6,41 @@ import java.util.*;
 // https://www.callicoder.com/three-sum-problem/
 public class threeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-        int target = 0;
+        ArrayList<List<Integer>> result = new ArrayList<>();
 
-        // base check ensure that nums.length > 3
-        if (nums.length < 3) {
-            return result;
-        }
         for (int i = 0; i < nums.length; i++) {
-            int currentTarget = target - nums[i];
-            Set<Integer> existingNums = new HashSet<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                if (existingNums.contains(currentTarget - nums[j])) {
-                    result.add(Arrays.asList(nums[i], nums[j], currentTarget - nums[j]));
-                } else {
-                    existingNums.add(nums[j]);
-                }
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
 
+            while (j < k) {
+                if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+                    k--;
+                    continue;
+                }
 
-        }return result;
+                if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    ArrayList<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    result.add(l);
+                    j++;
+                    k--;
+                }
+            }
+        }
 
+        return result;
     }
 
 
