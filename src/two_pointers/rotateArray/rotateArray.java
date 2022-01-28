2305dfa0ff4e9e7f4c54d5645c55e72ae809fda6
Stephen Cook <stevejcook114@gmail.com>
2022-01-28 12:52:34 -0500

    fixed a warning

--- src/two_pointers/rotateArray/rotateArray.java
+++ src/two_pointers/rotateArray/rotateArray.java
@@ -1,7 +1,46 @@
 package two_pointers.rotateArray;
 
 // https://leetcode.com/problems/rotate-array/
+// https://www.youtube.com/watch?v=BHr381Guz3Y
 public class rotateArray {
 
+    // first solution, uses an extra array to get the rotation
+    private static void rotate1(int[] nums, int k){
+        int n = nums.length;
+        k = k % n;
+        int[] sol = new int[n];
+
+        for(int i = 0; i < n; i++){
+            sol[(i+k)%n] = nums[i];
+        }
+        System.arraycopy(sol, 0, nums, 0, nums.length);
+    }
+
+    // reverses a segment of an array from lower to upper
+    private static void reverse(int[] nums, int lower, int upper){
+        int i, t;
+        for(i = lower; i < (upper+lower)/2; i++){
+            t = nums[i];
+            nums[i] = nums[(upper+lower)-i-1];
+            nums[(upper+lower)-i-1] = t;
+        }
+    }
+
+    // This solution uses array reversal
+    private static void rotate2(int[] nums, int k){
+         int n  = nums.length;
+         k = k % n;
+         reverse(nums,0,n);
+         reverse(nums,0,k);
+         reverse(nums,k,n);
+    }
+
+
+
+
+
+    public static void rotate(int[] nums, int k){
+        rotate2(nums,k);
+    }
 
 }
--- src/two_pointers/rotateArray/rotateArrayTest.java
+++ src/two_pointers/rotateArray/rotateArrayTest.java
@@ -1,13 +1,59 @@
 package two_pointers.rotateArray;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class rotateArrayTest {
+
+    // constraints
+    // 1 <= nums.length <= 10^5
+    // -2^31 <= nums[i] <= 2&31-1
+    // 0 <= k <= 10^5
 
     @Test
     public void  testRotateArrayValid1(){
         int[] nums = new int[]{1,2,3,4,5,6,7};
         int k = 3;
         int[] sol = new int[]{5,6,7,1,2,3,4};
+        rotateArray.rotate(nums,k);
+        assertArrayEquals(nums,sol);
+    }
+
+    @Test
+    public void testRotateArrayValid2(){
+        int[] nums = new int[]{-1,-100,3,99};
+        int k = 2;
+        int[] sol = new int[]{3,99,-1,-100};
+        rotateArray.rotate(nums,k);
+        assertArrayEquals(nums,sol);
+    }
+
+    @Test
+    public void testRotateArrayLen1(){
+        int[] nums = new int[]{1};
+        int k = 3;
+        int[] sol = new int[]{1};
+        rotateArray.rotate(nums,k);
+        assertArrayEquals(nums,sol);
+    }
+
+    @Test
+    public void testRotateArrayK0(){
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int k = 0;
+        int[] sol = new int[]{1,2,3,4,5,6,7,8,9,10};
+        rotateArray.rotate(nums,k);
+        assertArrayEquals(nums,sol);
+    }
+
+    @Test
+    public void testRotateArrayKGreaterThanN(){
+        int[] nums = new int[]{1,2,3,4,5};
+        int k = 6;
+        int[] sol = new int[]{5,1,2,3,4};
+        rotateArray.rotate(nums,k);
+        assertArrayEquals(nums,sol);
     }
 }
